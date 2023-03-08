768add44199c05f2ef125c5fb32859e2857ec2fa
hishamAbuodeh <hisham.ibrahim1899@gmail.com>
2023-03-08 11:35:58 +0300

    edit

--- Screen/Components/CardList.js
+++ Screen/Components/CardList.js
@@ -1,4 +1,5 @@
-import { FlatList } from "react-native"
+import { FlatList,TouchableOpacity,StyleSheet,Text,View } from "react-native"
+import SwipeableFlatList from 'react-native-swipeable-list'
 import CardLayout from "./CardLayout"
 import { Dimensions, RefreshControl } from "react-native"
 import { useState } from "react";
@@ -12,22 +13,63 @@ export default CardList = ({ data, token, setIsLoading, getUserOrders, navigatio
         getUserOrders(token)
     }
 
+    const extractItemKey = (item) => {
+        return `${item.id}`;
+    };
+
     const renderItem = (item,index) => {
         return(
             <CardLayout record={item} index={index} navigation={navigation} setOrderNo={setOrderNo}/>
         )
     }
 
+    const call = async(item) => {
+        alert('called')
+    }
+
+    const QuickActions = (index, item) => {
+        return (
+          <View style={[styles.qaContainer,{paddingTop:index==0? 30 : 0}]}>
+            <TouchableOpacity style={[styles.button]} onPress={() => call(item)}>
+                {/* <Feather name="edit" size={30} color="#fff" /> */}
+                <Text>
+                    Call
+                </Text>
+            </TouchableOpacity>
+          </View>
+        );
+      };
+
     return (
-        <FlatList 
+        // <FlatList 
+        //     style={{
+        //         width:windowWidth,
+        //         backgroundColor:'#EEEEEE'
+        //     }}
+        //     data={data}
+        //     renderItem={({item, index}) => renderItem(item,index)}
+        //     keyExtractor={item => item.no}
+        //     refreshControl={
+        //         <RefreshControl
+        //             refreshing={refreshing}
+        //             onRefresh={refresh}
+        //         />
+        //     }
+        // />
+        <SwipeableFlatList
             style={{
                 width:windowWidth,
                 backgroundColor:'#EEEEEE'
             }}
+            keyExtractor={extractItemKey}
             data={data}
-            renderItem={({item, index}) => renderItem(item,index)}
-            keyExtractor={item => item.no}
-            refreshControl={
+            renderItem={({item,index}) => (
+              <CardLayout record={item} index={index} navigation={navigation} setOrderNo={setOrderNo}/>
+            )}
+            maxSwipeDistance={120}
+            renderQuickActions={({index, item}) => QuickActions(index, item)}
+            shouldBounceOnMount={true}
+                refreshControl={
                 <RefreshControl
                     refreshing={refreshing}
                     onRefresh={refresh}
@@ -36,3 +78,25 @@ export default CardList = ({ data, token, setIsLoading, getUserOrders, navigatio
         />
     )
 }
+
+const styles = StyleSheet.create({
+    qaContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        marginBottom:5,
+        width:'97%',
+        backgroundColor:'#fff',
+        borderBottomRightRadius:10,
+        borderTopRightRadius:10
+    },
+    button: {
+        height:'100%',
+        width: 120,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor:'green',
+        borderBottomRightRadius:10,
+        borderTopRightRadius:10
+    },
+})
